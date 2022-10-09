163233f49b569f7198d384cf81f81256e1d4b6a9
James Sumners <james@sumners.email>
2022-10-09 14:16:50 -0400

    Some cleanups

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const BerReader = require('./lib/ber/reader')
 const BerWriter = require('./lib/ber/writer')
+const BerTypes = require('./lib/ber/types')
 
 module.exports = {
   BerReader,
+  BerTypes,
   BerWriter
 }
