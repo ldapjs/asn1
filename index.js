2e86f34fcb37d06de931e19126eec2ee9c9faefe
James Sumners <james@sumners.email>
2023-02-20 09:29:14 -0500

    Add hex dump utils

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const BerReader = require('./lib/ber/reader')
 const BerWriter = require('./lib/ber/writer')
 const BerTypes = require('./lib/ber/types')
+const bufferToHexDump = require('./lib/buffer-to-hex-dump')
 
 module.exports = {
   BerReader,
   BerTypes,
-  BerWriter
+  BerWriter,
+  bufferToHexDump
 }
